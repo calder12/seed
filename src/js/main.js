0384b3fb83482e83d0487abcc306c5c614a84fed
ItsJonQ <itsjonq@gmail.com>
2017-04-25 21:28:40 -0400

    Add scrolling to doc sidebar when appropriate

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 // Main
 'use strict';
 var SeedPackList = require('./components/seed-pack-list.js');
+var rxs = require('./vendor/rxs');
 
 (function($) {
   require('./utils/smoothScroll.js');
@@ -10,4 +11,20 @@ var SeedPackList = require('./components/seed-pack-list.js');
       seedPackList: new SeedPackList()
     }
   };
+
+  var $spySidebar = $('.js-spy-sidebar');
+  if ($spySidebar.length) {
+    var rxSidebarNavWidth = rxs('.rx-sidebar-nav-width');
+
+    var rxsResize = function() {
+      rxSidebarNavWidth.set({
+        width: $spySidebar.width() + 'px',
+      });
+    };
+
+    $(window).on('resize', rxsResize);
+
+    rxsResize();
+  }
+
 })(jQuery);
